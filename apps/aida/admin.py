58d9117825c6696646bb02c783e798296974adfc
Zaid Neurothrone <54468049+neurothrone@users.noreply.github.com>
2021-10-08 21:06:09 +0200

    setup admin interface for aida app

--- apps/aida/admin.py
+++ apps/aida/admin.py
@@ -1,3 +1,37 @@
 from django.contrib import admin
 
-# Register your models here.
+from apps.aida.models.training.weight import WeightTraining
+from apps.aida.models.training.exercise.weight import WeightExercise
+from apps.aida.models.training.exercise.set import Set
+
+
+class WeightExerciseInline(admin.TabularInline):
+    can_delete = True
+    model = WeightExercise
+    extra = 0
+
+
+class SetInline(admin.TabularInline):
+    can_delete = True
+    model = Set
+    extra = 0
+
+
+@admin.register(WeightTraining)
+class WeightTrainingAdmin(admin.ModelAdmin):
+    list_display = ("__str__", "exercised_on")
+    inlines = (WeightExerciseInline,)
+
+
+@admin.register(WeightExercise)
+class WeightExerciseAdmin(admin.ModelAdmin):
+    list_display = ("name", "weight", "vest_weight")
+    inlines = (SetInline,)
+
+
+@admin.register(Set)
+class SetAdmin(admin.ModelAdmin):
+    list_display = ("name", "reps")
+
+    def name(self, obj):
+        return obj.exercise.name
