191fd777db8828473d7c0187767dab54fc95155b
Zaid Neurothrone <54468049+neurothrone@users.noreply.github.com>
2021-10-08 21:01:05 +0200

    add a base model

---
+++ shared/models/base.py
@@ -0,0 +1,12 @@
+import uuid
+
+from django.db import models
+
+
+class BaseModel(models.Model):
+    id = models.UUIDField(default=uuid.uuid4, editable=False, unique=True, primary_key=True)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
