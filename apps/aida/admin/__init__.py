b45c6b5e6c2582b4f759c52e38793b56964730cd
Zaid Neurothrone <54468049+neurothrone@users.noreply.github.com>
2021-10-15 14:46:06 +0200

    fix admin interface

--- apps/aida/admin/__init__.py
+++ apps/aida/admin/__init__.py
@@ -1,4 +1,7 @@
 from apps.aida.admin.activity.base import WorkoutAdmin
+from apps.aida.admin.activity.exercise import ExerciseAdmin
+from apps.aida.admin.activity.set import CardioSetAdmin
+from apps.aida.admin.activity.set import WeightSetAdmin
 
 from apps.aida.admin.health.sleep import SleepAdmin
 from apps.aida.admin.health.pulse import HeartRateAdmin
--- apps/aida/admin/activity/base.py
+++ apps/aida/admin/activity/base.py
@@ -6,6 +6,7 @@ from apps.aida.models.activity.workout import Workout
 
 @admin.register(Workout)
 class WorkoutAdmin(admin.ModelAdmin):
+    fields = ("type", "engaged_at")
     list_display = ("type", "engaged_at")
     inlines = (ExerciseInline,)
     list_filter = ("type", "engaged_at")
