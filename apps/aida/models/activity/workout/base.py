ac24925aaa112c1796b2e658762180f380d6cd56
Zaid Neurothrone <54468049+neurothrone@users.noreply.github.com>
2021-10-15 14:16:00 +0200

    add create() factory method to Workout model

--- apps/aida/models/activity/workout/base.py
+++ apps/aida/models/activity/workout/base.py
@@ -1,4 +1,7 @@
+from datetime import datetime
+
 from django.db import models
+from django.utils.timezone import make_aware
 
 from ..base import Activity
 
@@ -10,10 +13,25 @@ WORKOUT_TYPES = [
 
 class Workout(Activity):
     type = models.CharField(choices=WORKOUT_TYPES, max_length=255, blank=True, null=True)
-    trained_at = models.DateTimeField(blank=True, null=True)
 
     def __str__(self) -> str:
-        return f"{self.type.title()} | {self.trained_at.date()}"
+        return f"{self.type.title()} | {self.engaged_at.date()}"
 
     class Meta:
-        ordering = ("-trained_at",)
+        ordering = ("-engaged_at",)
+
+    @staticmethod
+    def create(workout_type: str, engaged_at: str) -> "Workout":
+        """Creates a workout object, stores it in the database and returns the object.
+
+        Args:
+            workout_type (str): type of workout, can be <cardio> or <weight>.
+            engaged_at (str): str to format into datetime object with format YYYY-MM-DD HH:MM.
+
+        Returns:
+            Workout: the workout object that was created.
+        """
+
+        aware_dt = make_aware(datetime.strptime(engaged_at, "%Y-%m-%d %H:%M"))
+        return Workout.objects.create(type=workout_type, engaged_at=aware_dt)
+
