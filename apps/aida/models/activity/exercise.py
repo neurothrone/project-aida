0c2c3ce9fe1219a87597691bfd92035e13a8733e
Zaid Neurothrone <54468049+neurothrone@users.noreply.github.com>
2021-10-19 22:03:20 +0200

    make exercise model inherit ViewUrlsMixin

--- apps/aida/models/activity/exercise.py
+++ apps/aida/models/activity/exercise.py
@@ -2,6 +2,7 @@ from django.db import models
 
 from apps.aida.models.activity.workout import Workout
 from shared.models.base import BaseModel
+from shared.models.urls import ViewUrlsMixin
 
 EXERCISE_TYPES = [
     ("c", "Cardio"),
@@ -10,7 +11,7 @@ EXERCISE_TYPES = [
 ]
 
 
-class Exercise(BaseModel):
+class Exercise(BaseModel, ViewUrlsMixin):
     workout = models.ForeignKey(Workout, on_delete=models.CASCADE,
                                 help_text="The workout the exercise belongs to")
     type = models.CharField(choices=EXERCISE_TYPES, max_length=25,
@@ -22,3 +23,15 @@ class Exercise(BaseModel):
 
     def __str__(self) -> str:
         return self.name
+
+    @property
+    def detail_url(self) -> str:
+        return "aida:exercise-detail"
+
+    @property
+    def update_url(self) -> str:
+        return "aida:exercise-update"
+
+    @property
+    def delete_url(self) -> str:
+        return "aida:exercise-delete"
