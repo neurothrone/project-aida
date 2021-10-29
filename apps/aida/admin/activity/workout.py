49d1cdc6fef2feea18a445f9af1b17d8092b4161
Zaid Neurothrone <54468049+neurothrone@users.noreply.github.com>
2021-10-29 02:14:36 +0200

    create base activity admin class for engaged_at table format

--- apps/aida/admin/activity/workout.py
+++ apps/aida/admin/activity/workout.py
@@ -1,12 +1,13 @@
 from django.contrib import admin
 
+from apps.aida.admin.activity import ActivityMetricAdmin
 from apps.aida.admin.activity.inlines import ExerciseInline
 from apps.aida.models.activity.workout import Workout
 
 
 @admin.register(Workout)
-class WorkoutAdmin(admin.ModelAdmin):
+class WorkoutAdmin(ActivityMetricAdmin):
     fields = ("type", "engaged_at")
-    list_display = ("type", "engaged_at")
+    list_display = ("id", "type", "engaged_at_")
     inlines = (ExerciseInline,)
     list_filter = ("type", "engaged_at")
