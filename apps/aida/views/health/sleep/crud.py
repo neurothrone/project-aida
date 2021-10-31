ea73c6448f807a5b58cfb3f71bb842c8291c5858
Zaid Neurothrone <54468049+neurothrone@users.noreply.github.com>
2021-10-31 12:41:45 +0100

    remove commented out code

--- apps/aida/views/health/sleep/crud.py
+++ apps/aida/views/health/sleep/crud.py
@@ -21,7 +21,6 @@ class List(generic.ListView):
     def get_context_data(self, **kwargs):
         context = super(List, self).get_context_data(**kwargs)
         context["title"] = "Sleep Data"
-        # context["form"] = LoadLocalDataForm()
         return context
 
 
